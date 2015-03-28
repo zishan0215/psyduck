716ac20993ad51a749311f711e801bef7af3b850
Antriksh <antriksh@antriksh.(none)>
2015-03-28 19:23:15 +0530

    added gui to both client and server

--- psyduck/client.py
+++ psyduck/client.py
@@ -2,6 +2,8 @@ import pyaudio
 import socket
 import sys
 import time
+from PyQt4 import QtGui, QtCore, uic
+import sys
 
 # Pyaudio Initialization
 chunk = 1024
@@ -19,7 +21,7 @@ stream = p.open(format = FORMAT,
 
 # Socket Initialization
 host = 'localhost'
-port = 50001
+port = 50000
 size = 1024
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host,port))
@@ -27,9 +29,20 @@ s.connect((host,port))
 # Main Functionality
 while 1:
     data = stream.read(chunk)
-    s.send(data)
-    s.recv(size)
+    
+    if __name__ == '__main__':
+		app = QtGui.QApplication(sys.argv)
+		app.setStyle("cleanlooks")
+
+		listwidget = QtGui.QListWidget()
+		listwidget.show()
+
+		s.send(data)
+   		s.recv(size)
+
+		sys.exit(app.exec_())
 
 s.close()
 stream.close()
 p.terminate()
+
--- psyduck/server.py
+++ psyduck/server.py
@@ -1,6 +1,8 @@
 import pyaudio
 import socket
 import sys
+from PyQt4 import QtGui, QtCore, uic
+import sys
 
 # Pyaudio Initialization
 chunk = 1024
@@ -17,7 +19,7 @@ stream = p.open(format = FORMAT,
 
 # Socket Initialization
 host = ''
-port = 50001
+port = 50000
 backlog = 5
 size = 1024
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -29,11 +31,22 @@ client, address = s.accept()
 # Main Functionality
 while 1:
     data = client.recv(size)
-    if data:
-        # Write data to pyaudio stream
-        stream.write(data)  # Stream the recieved audio data
-        client.send('ACK')  # Send an ACK
+    
+    if __name__ == '__main__':
+		app = QtGui.QApplication(sys.argv)
+		app.setStyle("cleanlooks")
+
+		listwidget = QtGui.QListWidget()
+		listwidget.show()
+
+		if data:
+        	#Write data to pyaudio stream
+	        stream.write(data)  # Stream the recieved audio data
+	        client.send('ACK')  # Send an ACK
+
+		sys.exit(app.exec_())
 
 client.close()
 stream.close()
 p.terminate()
+
